2d93ae69474b1eb9e139330e6855572367c5361f
Tatiana Ivanova <tati.ivanova.87@gmail.com>
2024-01-08 17:08:04 +0300

    fix

---
+++ dist/next.config.dev.js
@@ -0,0 +1,9 @@
+"use strict";
+
+/** @type {import('next').NextConfig} */
+var nextConfig = {
+  images: {
+    domains: ['localhost', 'https://img.clerk.com', 'img.clerk.com']
+  }
+};
+module.exports = nextConfig;
